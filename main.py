0dc16590e7d64af5034069c11c5019d9812460cd
github_MuhammaduBarry <Muhammadubarry5@gmail.com>
2025-01-13 12:57:03 -0500

    Added functionality to PyScrappie

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    pass
+from bs4 import BeautifulSoup
+import pprint
+
+from src.scrape import html_page_one_parser, html_page_two_parser, create_custom_hn, sort_stories_by_vote
+
+def main() -> None:
+    soup: BeautifulSoup = html_page_one_parser()
+    links: list = soup.select(".titleline > a")
+    subtext: list = soup.select(".subtext")
+
+    soup_two: BeautifulSoup = html_page_two_parser()
+    links_two: list = soup_two.select(".titleline > a")
+    subtext_two: list = soup_two.select(".subtext")
+
+    both_pages = create_custom_hn(links, subtext) + create_custom_hn(links_two, subtext_two)
+    both_pages = sort_stories_by_vote(both_pages)
+    pprint.pprint(both_pages)
+
 
 if __name__ == '__main__':
     main()
--- src/scrape.py
+++ src/scrape.py
@@ -2,6 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 
 
-r = requests.get("https://news.ycombinator.com/news")
-soup = BeautifulSoup(r.text, "html.parser")
+def html_page_one_parser() -> BeautifulSoup:
+    # Return Response Code
+    r: requests.Response = requests.get("https://news.ycombinator.com/news")
+    # Parse text in html form
+    soup: BeautifulSoup = BeautifulSoup(r.text, "html.parser")
 
+    return soup
+
+def html_page_two_parser() -> BeautifulSoup:
+    r: requests.Response = requests.get("https://news.ycombinator.com/news?p=2")
+    soup: BeautifulSoup = BeautifulSoup(r.text, 'html.parser')
+
+    return soup
+
+def sort_stories_by_vote(hnlist: list) -> list:
+    return sorted(hnlist, key=lambda k:k["Vote"], reverse=True)
+
+def create_custom_hn(links: list, subtext: list) -> list:
+    hn: list = []
+
+    for i, item in enumerate(links):
+        title: str = links[i].getText()
+        href: str = links[i].get("href", None)
+        vote: list = subtext[i].select(".score")
+
+        if len(vote):
+            points: int = int(vote[0].getText().replace(" points", ""))
+            if points > 100:
+                hn.append({"Title": title, "Link": href, "Vote": points})
+
+    return sort_stories_by_vote(hn)
